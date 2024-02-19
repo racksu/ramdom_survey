3d2d2d591f9e7abfa0818f55539b310c24b89b3a
racksu <58040993+racksu@users.noreply.github.com>
2024-02-20 07:52:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var urls = [
 	'https://www.surveycake.com/s/ekNmB',
 	//'https://www.surveycake.com/s/rxMWo',
 	'https://www.surveycake.com/s/mo8O9',
-	'https://www.surveycake.com/s/MZzwe',
+	//'https://www.surveycake.com/s/MZzwe',
 	'https://www.surveycake.com/s/12Rpl',
 	//'https://www.surveycake.com/s/NZQWx'
 	
